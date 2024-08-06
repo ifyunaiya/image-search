f0178dc1053d908ff61e32488e75604b9cf74b68
ifyunaiya <nwankwo.angie@gmail.com>
2024-08-06 23:25:22 +0100

    First commit

---
+++ dist/index.js
@@ -0,0 +1,67 @@
+const button = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+const inputfield = document.getElementById('search-input');
+let character = inputfield.placeholder;
+const url = `https://api.unsplash.com/search/photos?page=1&client_id=2TMZt136qJJby3z1aUwzpcwPOd-WA_ENITeJ_br5oxE&query=${character}`;
+
+button.addEventListener('click',()=>{
+    menu.classList.toggle('hidden')
+})
+
+
+async function getData(charactertyped){
+    let newurl = `https://api.unsplash.com/search/photos?page=1&client_id=2TMZt136qJJby3z1aUwzpcwPOd-WA_ENITeJ_br5oxE&query=${charactertyped}`;
+    const response = await fetch(newurl);
+    const data = await response.json();
+    console.log(data)
+    return data
+}
+
+inputfield.addEventListener('input',(e) =>{
+    let charactertyped = e.target.value;
+    getData(charactertyped);
+    displayImages(charactertyped)
+})
+
+async function displayImages(charactertyped){
+    let imagetodisplay = await getData(charactertyped);
+    let datadisplay = imagetodisplay.results.map((object) => {
+       
+        const {urls} = object;
+        return `
+        <div class = "container">
+        <img src = ${object.urls.small}>
+        </div>
+        `
+    }).join("");
+
+    display.innerHTML = datadisplay;
+    /* console.log(display) */
+    
+}
+
+displayImages()
+
+
+/* async function getPhotos(character) {
+   
+    fetch(url)
+     .then(function(response){
+        return response.json();
+        
+     })
+     .then(function(data){
+        console.log(data)
+        data.results.map(data => console.log(data.urls.regular))
+        console.log(data.results[0])
+        const imageElement = document.createElement("img");
+        const display = document.getElementById('display');
+        imageElement.src = data.results[0].urls.regular
+        display.appendChild(imageElement);
+     })
+     .catch((error) => console.error(error));
+        
+    
+    }
+
+    */
